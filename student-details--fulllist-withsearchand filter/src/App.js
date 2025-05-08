96fd7606ec56e711772df0f1f9891df43c814d4a
DESKTOP-6FDPGVD\admin <rk.channagiri@gmail.com>
2025-05-08 14:22:33 +0530

    student list with ID name search

--- student-details--fulllist-withsearchand filter/src/App.js
+++ student-details--fulllist-withsearchand filter/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { studentList } from './data.js';
 import "./App.css";
 
@@ -6,25 +6,10 @@ export default function Gallery() {
   const [index, setIndex] = useState(0);
   const [searchItem, setSearchItem] = useState('')
   const [selectedValue, setSelectedValue] = useState('ID');
+  const [Student, setStudent] = useState(studentList);
 
-
-
-  function handleNextClick() {
-    if (index == studentList.length - 1) {
-      setIndex(0);
-    } else {
-      setIndex(index + 1);
-    }
-  }
-
-  function handlePreviousClick() {
-    if (index > 0) {
-      setIndex(index - 1);
-    } else {
-      setIndex(studentList.length - 1);
-    }
-  }
-
+  const [filteredStudent, setFilteredStudent] = useState(studentList);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleInputChange = (e) => {
     const searchTerm = e.target.value;
@@ -32,26 +17,19 @@ export default function Gallery() {
   }
 
 
-
   const SearchStudent = (e) => {
-    const searchTerm = e.target.value;
-    console.log("searchTerm", searchTerm);
-    for (var i = 0; i < studentList.length; i++) {
-      // console.log(studentList.at(i).name);
-      if(selectedValue == "ID"){
-        if (studentList.at(i).ID == searchTerm) {
-          setIndex(i);
-          return;
-        }
-      }else{
-      
-      if (studentList.at(i).name == searchTerm) {
-        setIndex(i);
-        return;
-      }
+    // console.log(selectedValue, searchItem, studentList.filter(stud => stud.ID == searchItem))
+    if (selectedValue === 'ID') {
+      setFilteredStudent(studentList.filter(stud => stud.ID == searchItem));
+      console.log(selectedValue, searchItem, studentList.filter(stud => stud.ID == searchItem))
+    } else {
+      setFilteredStudent(studentList.filter(stud => stud.name == searchItem));
+      console.log(selectedValue, searchItem, studentList.filter(stud => stud.name == searchItem))
     }
-    }
-    alert("sorry");
+  }
+
+  const Resetfilter = () => {
+    setFilteredStudent(studentList);
   }
 
   let students = studentList[index];
@@ -59,7 +37,7 @@ export default function Gallery() {
   return (
     <>
       <div className='gallery-container'>
-        <table border="1" width="100%">
+        <table border="1" width="100%" cellPadding={0} cellSpacing={0}>
           <tbody>
             <tr>
               <td style={{ textAlign: 'left', width: '10%' }}>
@@ -67,63 +45,56 @@ export default function Gallery() {
                   {index + 1} of {studentList.length}
                 </h3>
               </td>
-
-              <td style={{ textAlign: 'left', width: '10%' }}>
+              <td style={{ textAlign: 'center', width: '10%' }}>
                 <select style={{ width: "100px" }} defaultValue="ID" onChange={e => setSelectedValue(e.target.value)} value={selectedValue}
-                  >
+                >
                   <option value="ID">ID</option>
                   <option value="Name">Name</option>
-                  
+
                 </select>
               </td>
-
               <td style={{ textAlign: 'right', padding: '5px', width: '10%' }}>
                 <input type="text"
                   value={searchItem}
                   onChange={handleInputChange}
                   placeholder='Type to search' />
                 <button value={searchItem} onClick={SearchStudent}>Go</button>
+                <button onClick={Resetfilter}>Reset</button>
               </td>
             </tr>
           </tbody>
         </table>
-        <table border="1" width="100%">
+        <table border="1" width="100%" cellPadding={0} cellSpacing={0}>
           <thead>
             <tr>
               <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>Student ID</th>
               <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>Student Name</th>
               <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>SurName</th>
-              <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>Student Image
-              </th>
+              <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>Student Image</th>
               <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>Marks-1</th>
               <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>Marks-2</th>
               <th style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>Marks-3</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}><i>{students.ID} </i></td>
-              <td style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}><i>{students.name} </i></td>
-              <td style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}><i>{students.surname} </i></td>
-              <td style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}>
-                <img
+            {/*   {filteredUsers.map(user => ( */}
+            {filteredStudent.map((students, i) => (
+              <tr key={i}>
+                <td>{students.ID}</td>
+                <td>{students.name}</td>
+                <td>{students.surname}</td>
+                <td> <img
                   src={students.url}
                   alt={students.alt}
-                />
-              </td>
-              <td style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}><i>{students.marks1} </i></td>
-              <td style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}><i>{students.marks2} </i></td>
-              <td style={{ textAlign: 'center', fontSize: "12px", padding: '5px', width: '10%' }}><i>{students.marks3} </i></td>
-            </tr>
+                /></td>
+                <td>{students.marks1}</td>
+                <td>{students.marks2}</td>
+                <td>{students.marks3}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <br />
-        <button onClick={handlePreviousClick}>
-          Previes
-        </button>
-        <button onClick={handleNextClick}>
-          Next
-        </button>
       </div >
     </>
   );

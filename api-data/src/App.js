baf65a62a6c7389c75257d9d5a1f04dba5d538ab
DESKTOP-6FDPGVD\admin <rk.channagiri@gmail.com>
2025-06-18 17:48:23 +0530

    api data

--- api-data/src/App.js
+++ api-data/src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   const [posts, setPosts] = useState([]);
 
-   useEffect(() => {
+  useEffect(() => {
     fetch('https://dummyjson.com/users')
       .then((response) => {
-        
+
         if (!response.ok) {
           throw new Error('Network response was not OK');
         }
@@ -17,34 +17,39 @@ function App() {
       })
       .then((data) => setPosts(data.users))
       .catch((error) => console.error('Fetch error:', error));
-     
+
   }, []);
 
- 
 
-  
+
+
   return (
-        
-    <>{console.log("User at 0 :", posts.at(0))}
-    
-    <div className="App">
-      <h1>API Data Display</h1>
-       <div>
-      {posts.map((usr) => (
-        
-        <div key={usr.id} style={{ marginBottom: '20px' }}>
-          <ul style={{textAlign:'left'}}>
-          <li><h3>{usr.id}</h3><h3>{usr.firstName}</h3></li>
-           
-          <img src={usr.image} style={{width:'10%', height:'10%'}} />
-         
-          </ul>
+
+    <>
+      {console.log("User at 0 :", posts.at(0))}
+
+      <div className="App">
+        <h1>API Data Display</h1>
+        <div>
+          {posts.map((usr) => (
+
+            <div key={usr.id}>
+              <div style={{border:'1px solid #ccc',backgroundColor:'#f7f7f7', borderRadius:'15px', width:'23%', margin:'0.5%', display:"inline-block", float:'left'}}>
+                <ul style={{ textAlign: 'left' }}>
+                  <img src={usr.image} style={{ width: '20%', height: '20%' }} />
+                  <li><h3>{usr.id}</h3><h3>{usr.firstName} - {usr.maidenName} - {usr.lastName} <br /> Age - {usr.age}</h3></li>
+                  <li><h3>{usr.address.address}</h3><h3>{usr.address.city} - {usr.address.state} - {usr.address.stateCodes} - {usr.age}</h3></li>
+
+                  
+
+                </ul>
+              </div>
+            </div>
+
+          ))}
         </div>
-        
-      ))}
-    </div>
-    </div>
-</>
+      </div>
+    </>
 
   );
 }
